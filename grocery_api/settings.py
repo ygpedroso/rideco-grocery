e7188cbf8f7fc82cc8344dfed1e2f0d69e13ba8c
ygpedroso <ygpedroso2503@gmail.com>
2018-08-06 02:18:03 -0400

    Add the schema to the Graphene configuration

--- grocery_api/settings.py
+++ grocery_api/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+GRAPHENE = {
+    'SCHEMA': 'api.schema'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
