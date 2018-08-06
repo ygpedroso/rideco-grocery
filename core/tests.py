ca4ae519fa002a3baefb602fbbde1b9083829e88
ygpedroso <ygpedroso2503@gmail.com>
2018-08-06 15:45:34 -0400

    Test the string representation of the Product model

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductModelTest(TestCase):
+
+    def test_string_representation(self):
+        product = Product(name="Buy eggs")
+        self.assertEqual(str(product), product.name)
