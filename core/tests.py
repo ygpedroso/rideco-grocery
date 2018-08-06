5a1b25b6cf9a8e27843bd533dae766e77e2cd546
ygpedroso <ygpedroso2503@gmail.com>
2018-08-06 15:49:39 -0400

    Test _meta.verbose_name_plural of the Product model

--- core/tests.py
+++ core/tests.py
@@ -7,3 +7,6 @@ class ProductModelTest(TestCase):
     def test_string_representation(self):
         product = Product(name="Buy eggs")
         self.assertEqual(str(product), product.name)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Product._meta.verbose_name_plural), "products")
