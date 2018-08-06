3f522402134746592b9e4cbea3901cf89ff00118
ygpedroso <ygpedroso2503@gmail.com>
2018-08-06 12:53:43 -0400

    Test CreateProduct mutation

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from graphene.test import Client
+from core.models import Product
+from api.schema import schema
 
-# Create your tests here.
+
+class ProductSchemaMutationTest(TestCase):
+    def setUp(self):
+        Product.objects.create(id=1, name="Buy milk")
+        Product.objects.create(id=2, name="Pick up Tomatoes")
+
+    @staticmethod
+    def test_create_product():
+        client = Client(schema)
+        executed = client.execute('''mutation{createProduct(name: "Buy Soda"){success errors product{id name}}}''')
+        assert executed['data']['createProduct']['success'] is True
+        assert Product.objects.count() == 3
+
