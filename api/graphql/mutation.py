4da2823b1910e5a5d71b1b7fc76e509d084866e1
ygpedroso <ygpedroso2503@gmail.com>
2018-08-06 02:15:33 -0400

    Create GraphQL Mutation for removing a Product

--- api/graphql/mutation.py
+++ api/graphql/mutation.py
@@ -16,5 +16,18 @@ class CreateProduct(graphene.Mutation):
         return CreateProduct(product=product)
 
 
+class RemoveProduct(graphene.Mutation):
+    product = graphene.Field(ProductType)
+
+    class Arguments:
+        product_id = graphene.ID(required=True)
+
+    def mutate(self, info, product_id):
+        product = Product.objects.get(id=product_id)
+        product.delete()
+        return RemoveProduct(product=product)
+
+
 class Mutation(graphene.ObjectType):
     create_product = CreateProduct.Field()
+    remove_product = RemoveProduct.Field()
