ca20125353fa42db32bf30ee345afa31f3bc8e9a
ygpedroso <ygpedroso2503@gmail.com>
2018-08-07 12:33:41 -0400

    Disable csrf for the graphql endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('', GraphQLView.as_view(graphiql=True)),
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
